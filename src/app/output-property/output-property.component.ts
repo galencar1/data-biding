91423a571d7a90db47b87ab9d4a157aae3134310
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-18 08:34:50 -0300

    Builds

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -9,28 +9,26 @@ export class OutputPropertyComponent implements OnInit {
   @Input() valor: number = 0;
   @Output() mudouValor = new EventEmitter(); // Emissor de Eventos
 
-  @ViewChild('#campoInput')  campoValorInput!: ElementRef;
+ 
 
   
   incrementa(){
-    this.campoValorInput.nativeElement.value++;
+    this.valor++;
     this.mudouValor.emit({novoValor: this.valor})
   }
 
   
 
   decrementa(){
-    this.campoValorInput.nativeElement.value--;
+    this.valor--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   constructor() { 
-   this.initialize();
+   
   }
 
-  initialize(){
-    this.campoValorInput;
-  }
+ 
   
 
   ngOnInit(): void {
